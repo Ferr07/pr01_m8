9ccd6406a11f276191b0d9f2459dc124653b7fc5
Ferran <Ferran@FERRAN>
2016-03-27 14:05:47 +0200

    Primera modificacio Gihub.com

---
+++ nom pr01_m8_copia_01/src/nom/pr01_m8_copia_01/NomPr01_m8_copia_01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nom.pr01_m8_copia_01;
+
+/**
+ *
+ * @author Ferran
+ */
+public class NomPr01_m8_copia_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
