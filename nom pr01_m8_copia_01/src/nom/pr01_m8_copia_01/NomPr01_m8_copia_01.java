47f54771b88941e9e0ab776aa80c448255dc57c8
Ferran <Ferran@FERRAN>
2016-04-04 08:28:42 +0200

    versio 3.0 del proejcte pr01_m8p 01_m8

--- nom pr01_m8_copia_01/src/nom/pr01_m8_copia_01/NomPr01_m8_copia_01.java
+++ nom pr01_m8_copia_01/src/nom/pr01_m8_copia_01/NomPr01_m8_copia_01.java
@@ -5,6 +5,11 @@
  */
 package nom.pr01_m8_copia_01;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author Ferran
@@ -14,8 +19,28 @@ public class NomPr01_m8_copia_01 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+    File f = new File("fitxer.html");
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+        bw.write("<html>");bw.newLine();
+        bw.write(" <head>");bw.newLine();
+        bw.write(" <title>");bw.newLine();
+        bw.write("<h1>DAW2 Esto no es seri</h1>");
+        bw.newLine();
+        bw.write(" </title>");
+        bw.newLine();
+        bw.write(" </head>");
+        bw.newLine();
+        bw.write(" <body>");
+        bw.newLine();
+        bw.write(" <h1>DAW2 Esto no es serio</h1>");
+        bw.newLine();
+        bw.write(" </body>");
+        bw.newLine();
+        bw.write("</html>");
+        bw.newLine();
+        bw.close();
+        }
     }
     
 }
